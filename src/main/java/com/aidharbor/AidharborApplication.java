b6f7b31cc36d2c719c5e6a167e210520ad2a2563
qwer7824 <sh7824@naver.com>
2024-03-11 23:13:13 +0900

    초기 세팅

---
+++ src/main/java/com/aidharbor/AidharborApplication.java
@@ -0,0 +1,13 @@
+package com.aidharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidharborApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidharborApplication.class, args);
+    }
+
+}
