c25f9cbf960e6a43a6741b38b45d768038f94626
qwer7824 <sh7824@naver.com>
2024-03-13 22:36:09 +0900

    setting

---
+++ src/test/java/com/aidharbor/AidharborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidharborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
